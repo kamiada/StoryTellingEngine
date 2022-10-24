a172ab2a3eb0205ad28eeb33dcb2f33a9bd5a458
kamiada <kamiada528@gmail.com>
2022-10-24 09:42:34 +0100

    set up 5

--- src/Engine/Menu/Menu.jsx
+++ src/Engine/Menu/Menu.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Container } from './Menu.styles';
 
 export const Menu = () => {
-
+ return (
+    <Container>
+        Menu
+    </Container>
+ )
 }
 
 export default Menu;
